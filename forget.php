bb6c1bff5f4ab8eb0add738bd7a0c82f0005e201
Arkolips <catel.ludovic@gmail.com>
2016-07-05 11:42:44 +0200

    Update forget.php for configure new password and forget password

--- forget.php
+++ forget.php
@@ -2,15 +2,53 @@
   require 'config.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Mot de Passe oublié ?</title>
-    <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-  </head>
-  <body>
+<?php
+  if (isset($_GET['forgetToken'])) {
+    $forget = $_GET['forgetToken'];
+    $checkToken = $db->prepare("SELECT * FROM users WHERE forget = :forget");
+    $checkToken->bindValue(":forget", $forget, PDO::PARAM_STR);
+    $checkToken->execute();
+      if ($checkToken->rowCount()) { ?>
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title>Mot de Passe oublié ?</title>
+            <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+          </head>
+          <body>
+            <form class="checkToken" method="POST">
+              <div class="checkToken">
+                <label for="new-password">Nouveau Mot de Passe :</label>
+                <input type="password" name="new-password">
+              </div>
+              <div class="checkToken">
+                <label for="confirm-new-password">Confirmez le nouveau Mot de Passe :</label>
+                <input type="password" name="confirm-new-password">
+              </div>
+              <div class="checkToken">
+                <label for="checkTokenSubmit"></label>
+                <input type="submit" name="checkTokenSubmit" value="Confirmer">
+              </div>
+              <?php
+                if (isset($_POST['checkTokenSubmit'])) {
+                  $user = $checkToken->fetch();
+                  $password = password_hash(trim($_POST['new-password']), PASSWORD_BCRYPT);
+                  $query = $db->prepare("UPDATE users SET password = :password WHERE id = :id");
+                  $query->bindValue(':password', $password, PDO::PARAM_STR);
+                  $query->bindValue(':id', $user['id'], PDO::PARAM_INT);
+                  if ($query->execute()) {
+                    echo "Votre mot de passe à été mis à jour";
+                  }
+                }
+              ?>
+            </form>
+    <?php  }else {
+          echo "Erreur";
+    }
+  }
+  else { ?>
     <div id="backgroundForget">
       <fieldset>
         <legend>Mot de Passe oublié</legend>
@@ -25,6 +63,9 @@
           </div>
         </form>
       </fieldset>
+<?php
+  }
+?>
       <?php
         if (isset($_POST['forgetSubmit'])) {
           $login = $_POST['forgetLogin'];
@@ -34,7 +75,7 @@
           if ($checkUser->rowCount()) {
             $forget = sha1(md5(uniqid().$_SERVER['REMOTE_ADDR']));
             $db->query("UPDATE users SET forget = '$forget' WHERE login = '".$login."'");
-            echo "<p class='answerForget'>Bonjour ".$login.", vous pouvez redéfinir votre Mot de Passe sur </p><a href='".$url."?forget=".$forget."'>".$url."?forget=".$forget."</a>";
+            echo "<p class='answerForget'>Bonjour ".$login.", vous pouvez redéfinir votre Mot de Passe sur </p><a href='".$url."?forgetToken=".$forget."'>".$url."?forgetToken=".$forget."</a>";
           }
           else {
             echo "L'utilisateur saisie n'existe pas.";
